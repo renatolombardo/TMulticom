bcc5be2807a322e3e562f553af7c63b42b6b6c58
Renato Lombardo <renato.lombardo@gmail.com>
2021-06-24 22:43:53 -0300

    Add project files.

---
+++ TMulticom.Domain/Models/Amigo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMulticom.Domain.Models
+{
+    public class Amigo : Entity
+    {
+
+        public const int MAX_NOME_AMIGO_TAMANHO = 255;
+
+        private IList<Jogo> _jogos;
+
+        public Amigo(string nome)
+        {
+            Nome = nome;
+            _jogos = new List<Jogo>();
+        }
+
+        [Required]
+        [MaxLength(MAX_NOME_AMIGO_TAMANHO)]
+        public string Nome { get; private set; }
+
+        public IReadOnlyCollection<Jogo> Jogos { get => _jogos.ToArray(); }
+
+        public void EmprestarJogo(Jogo jogo)
+        {
+            jogo.DataEmprestimo = DateTime.Now;
+            _jogos.Add(jogo);
+        }
+
+        public void DevolverJogo(Jogo jogo)
+        {
+            jogo.DataEmprestimo = null;
+            _jogos.Remove(jogo);
+        }
+
+
+    }
+}
---
+++ TMulticom.Domain/Models/Jogo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMulticom.Domain.Models
+{
+    public class Jogo : Entity
+    {
+        public const int MAX_NOME_JOGO_TAMANHO = 255;
+
+        public Jogo(string nome, DateTime? dataEmprestimo)
+        {
+            Nome = nome;
+            DataEmprestimo = dataEmprestimo;
+        }
+
+        [Required]
+        [MaxLength(MAX_NOME_JOGO_TAMANHO)]
+        public string Nome { get; set; }
+        public DateTime? DataEmprestimo { get; set; }
+        public Guid? AmigoId { get; set; }
+        
+        [ForeignKey("AmigoId")]
+        public Amigo Amigo { get; set; }
+
+
+    }
+}
